6d8fc7644a6d034ed8e7f90ef22877f9e74bcdfd
varun <varunnssaini@gmail.com>
2020-12-19 20:27:18 +0530

    First time deploying app

--- src/app.js
+++ src/app.js
@@ -12,34 +12,60 @@ import 'react-dates/lib/css/_datepicker.css';
 import { firebase } from './firebase/firebase';
 import LoadingPage from './components/LoadingPage';
 
-const store = configureStore();
+import './App.css';
+import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer.js';
+
 const jsx = (
-  <Provider store={store}>
-    <AppRouter />
-  </Provider>
+  <div className="App">
+      <PathfindingVisualizer></PathfindingVisualizer>
+  </div>
 );
-let hasRendered = false;
-const renderApp = () => {
-  if (!hasRendered) {
-    ReactDOM.render(jsx, document.getElementById('app'));
-    hasRendered = true;
-  }
-};
 
-ReactDOM.render(<LoadingPage />, document.getElementById('app'));
+ReactDOM.render(jsx, document.getElementById('app'))
 
-firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-    store.dispatch(login(user.uid));
-    store.dispatch(startSetExpenses()).then(() => {
-      renderApp();
-      if (history.location.pathname === '/') {
-        history.push('/dashboard');
-      }
-    });
-  } else {
-    store.dispatch(logout());
-    renderApp();
-    history.push('/');
-  }
-});
+// const store = configureStore();
+// const jsx = (
+//   <Provider store={store}>
+//     <AppRouter />
+//   </Provider>
+// );
+// let hasRendered = false;
+// const renderApp = () => {
+//   if (!hasRendered) {
+//     ReactDOM.render(jsx, document.getElementById('app'));
+//     hasRendered = true;
+//   }
+// };
+
+// ReactDOM.render(<LoadingPage />, document.getElementById('app'));
+
+// firebase.auth().onAuthStateChanged((user) => {
+//   if (user) {
+//     store.dispatch(login(user.uid));
+//     store.dispatch(startSetExpenses()).then(() => {
+//       renderApp();
+//       if (history.location.pathname === '/') {
+//         history.push('/dashboard');
+//       }
+//     });
+//   } else {
+//     store.dispatch(logout());
+//     renderApp();
+//     history.push('/');
+//   }
+// });
+
+// import React from 'react';
+// import './App.css';
+// import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <PathfindingVisualizer></PathfindingVisualizer>
+//     </div>
+//   );
+// }
+
+// export default App;
+
